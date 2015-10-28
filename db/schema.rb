efde129c74d375cf69730bdddb1963c6c834bbcb
mallowlabs <mallowlabs@gmail.com>
2015-10-28 22:22:21 +0900

    Changed: New item button position

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20150309143445) do
     t.datetime "updated_at",   null: false
   end
 
+  add_index "items", ["release_date"], name: "index_items_on_release_date"
+
   create_table "users", force: :cascade do |t|
     t.string   "nickname",   null: false
     t.string   "name",       null: false
